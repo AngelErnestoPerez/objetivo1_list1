c76a7822d67e5bec23d7c4199c540326ee81abae
Angel Pérez <angellopez.pl111@academico.ifsul.edu.br>
2022-07-21 10:36:58 -0300

    primeiro commit

---
+++ src/controller/CarroController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import model.Carro;
+
+public class CarroController {
+
+    public static void main(String[] args){
+        Carro carro = new Carro();
+        System.out.println(carro);
+    }
+}
---
+++ src/controller/ProdutoController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import model.Produto;
+
+public class ProdutoController {
+
+    public static void main(String[] args) {
+        Produto produto = new Produto();
+        System.out.println(produto);
+    }
+}
